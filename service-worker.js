16c79d33b882e079582e75c8527bc4b9230d3258
YuraVolk <yura.volkovskiy.2006@gmail.com>
2019-09-19 19:45:16 +0300

    Service Worker

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+let CACHE_NAME = 'sw-v1'
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => cache.addAll('./404.html'))
+  )
+})
+self.addEventListener('fetch', (event) => {
+  if (event.request.method === 'GET') {
+    event.respondWith(
+      caches.match(event.request)
+      .then((cached) => {
+        var networked = fetch(event.request)
+          .then((response) => {
+            let cacheCopy = response.clone()
+            caches.open(CACHE_NAME)
+              .then(cache => cache.put(event.request, cacheCopy))
+            return response;
+          })
+          .catch(() => caches.match(offlinePage));
+        return cached || networked;
+      })
+    )
+  }
+  return;
+});
