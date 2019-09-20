e6237e4dac003a547e807158866626dfb8ef7f45
YuraVolk <yura.volkovskiy.2006@gmail.com>
2019-09-20 13:46:57 +0300

    Github Service Worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'pwa';
+/*var cacheName = 'pwa';
 var filesToCache = [
   '/',
   '/index.html',
@@ -20,6 +20,52 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request, {ignoreSearch:true}).then(response => {
       return response || fetch(event.request);
+    })
+  );
+});*/
+
+
+var APP_PREFIX = 'PWA-Test_';    
+var VERSION = 'v_01';          
+var CACHE_NAME = APP_PREFIX + VERSION;
+var URLS = [                     
+  '/{PWA-Test}/',          
+  '/{PWA-Test}/index.html'   
+];
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        return request;
+      } else {
+        return fetch(e.request);
+      }
+    })
+  );
+});
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(URLS);
+    })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      })
+      cacheWhitelist.push(CACHE_NAME);
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(keyList[i]);
+        }
+      }));
     })
   );
 });
