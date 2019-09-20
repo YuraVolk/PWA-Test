6e991e1125a3eb1e3c2cfac89e6d66585c3adb35
YuraVolk <yura.volkovskiy.2006@gmail.com>
2019-09-20 11:42:59 +0300

    Reliable

--- service-worker.js
+++ service-worker.js
@@ -1,66 +1,25 @@
-let cacheName = 'pwa-v.1.0.0';
-
-function addToCache(cacheName, request, response) {
-  caches.open(cacheName)
-    .then(cache => { cache.put(request, response) });
-}
-
-function preCache() {
-  return caches.open(cacheName).then(cache => {
-    return cache.addAll([
-      '/',
-      'css/style.css'
-    ]);
-  });
-}
-
-self.addEventListener('install', (e) => {
-  console.log('[ServiceWorker] Installer');
-  e.waitUntil(preCache());
-});
-
-self.addEventListener('activate', (e) => {
-  console.log('[ServiceWorker] Activate');
+var cacheName = 'pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css'
+];
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
   e.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(keyList.map(key => {
-        if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache');
-          return caches.delete(key);
-        }
-      }));
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
     })
   );
 });
-
-self.addEventListener('fetch', (e) => {
-  let request = e.request,
-    acceptHeader = request.headers.get('Accept');
-
-  if (acceptHeader.indexOf('text/html') !== -1) {
-    e.respondWith(
-      fetch(request)
-        .then(response => {
-          if (response.ok)
-            addToCache(cacheName, request, response.clone());
-          return response;
-        })
-        .catch(() => {
-          return caches.match(request).then(response => {
-            return response;
-          })
-        })
-    );
-  } else if (request.url.indexOf('yuravolk.github.io') !== -1 && (request.url.indexOf('.js') !== -1 || request.url.indexOf('.css') !== -1)) {
-    e.respondWith(
-      caches.match(request)
-        .then(response => {
-          let fetchPromise = fetch(e.request).then(networkResponse => {
-            addToCache(cacheName, request, networkResponse.clone());
-            return networkResponse;
-          })
-          return response || fetchPromise;
-        })
-    );
-  }
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
