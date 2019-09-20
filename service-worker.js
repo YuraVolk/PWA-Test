43302ae7b8e16a5775464d89cf39fd467e001aee
YuraVolk <yura.volkovskiy.2006@gmail.com>
2019-09-20 11:24:29 +0300

    Service Worker

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,66 @@
-let CACHE_NAME = 'sw-v1'
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(cache => cache.addAll('404.html'))
-  )
-})
-self.addEventListener('fetch', (event) => {
-  if (event.request.method === 'GET') {
-    event.respondWith(
-      caches.match(event.request)
-      .then((cached) => {
-        var networked = fetch(event.request)
-          .then((response) => {
-            let cacheCopy = response.clone()
-            caches.open(CACHE_NAME)
-              .then(cache => cache.put(event.request, cacheCopy))
+let cacheName = 'pwa-v.1.0.0';
+
+function addToCache(cacheName, request, response) {
+  caches.open(cacheName)
+    .then(cache => { cache.put(request, response) });
+}
+
+function preCache() {
+  return caches.open(cacheName).then(cache => {
+    return cache.addAll([
+      '/',
+      'css/style.css'
+    ]);
+  });
+}
+
+self.addEventListener('install', (e) => {
+  console.log('[ServiceWorker] Installer');
+  e.waitUntil(preCache());
+});
+
+self.addEventListener('activate', (e) => {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache');
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  let request = e.request,
+    acceptHeader = request.headers.get('Accept');
+
+  if (acceptHeader.indexOf('text/html') !== -1) {
+    e.respondWith(
+      fetch(request)
+        .then(response => {
+          if (response.ok)
+            addToCache(cacheName, request, response.clone());
+          return response;
+        })
+        .catch(() => {
+          return caches.match(request).then(response => {
             return response;
           })
-          .catch(() => caches.match(offlinePage));
-        return cached || networked;
-      })
-    )
+        })
+    );
+  } else if (request.url.indexOf('yuravolk.github.io') !== -1 && (request.url.indexOf('.js') !== -1 || request.url.indexOf('.css') !== -1)) {
+    e.respondWith(
+      caches.match(request)
+        .then(response => {
+          let fetchPromise = fetch(e.request).then(networkResponse => {
+            addToCache(cacheName, request, networkResponse.clone());
+            return networkResponse;
+          })
+          return response || fetchPromise;
+        })
+    );
   }
-  return;
 });
